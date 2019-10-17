c8744d43f24c7b76f4acdbd614a72d2fe39bdbe1
Jiwon Cha <jc4va@virginia.edu>
2019-10-17 19:10:01 +0900

    crawl image

---
+++ csv_poster.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import os
+import urllib
+
+
+def image_scrape(posters_df):
+    base_url = "http://image.tmdb.org/t/p/w185/"
+    posters_count = posters.count()
+    os.chdir(os.getcwd() + "/posters_data")
+    for i in range(posters_count):
+        urllib.request.urlretrieve(base_url + posters_df[i][1:], posters_df[i][1:])
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('movies_metadata.csv')
+    posters = df['poster_path']
+    image_scrape(posters)
