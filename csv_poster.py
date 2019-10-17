829c173754851f74bd9d747a69622bad17d6a3ce
jc4va <31323449+jc4va@users.noreply.github.com>
2019-10-17 19:26:50 +0900

    Update csv_poster.py

--- csv_poster.py
+++ csv_poster.py
@@ -6,7 +6,7 @@ import urllib
 def image_scrape(posters_df):
     base_url = "http://image.tmdb.org/t/p/w185/"
     posters_count = posters.count()
-    # 먼저 directory 안에 posters_data라는 폴더를 만들어 
+    # 먼저 directory 안에 posters_data라는 폴더를 만들어 주세요! 
     os.chdir(os.getcwd() + "/posters_data")
     for i in range(posters_count):
         urllib.request.urlretrieve(base_url + posters_df[i][1:], posters_df[i][1:])
