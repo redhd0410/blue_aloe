81dadc8305f7d536abad25fa64c7c1a174734464
Jiwon Cha <jc4va@virginia.edu>
2019-10-23 15:29:33 +0900

    step

--- csv_poster.py
+++ csv_poster.py
@@ -5,7 +5,7 @@ import urllib
 
 def image_scrape(posters_df):
     base_url = "http://image.tmdb.org/t/p/w185/"
-    posters_count = posters.count()
+    posters_count = posters_df.count()
     # 먼저 directory 안에 posters_data라는 폴더를 만들어 주세요! 
     os.chdir(os.getcwd() + "/posters_data")
     for i in range(posters_count):
@@ -14,5 +14,5 @@ def image_scrape(posters_df):
 
 if __name__ == '__main__':
     df = pd.read_csv('movies_metadata.csv')
-    posters = df['poster_path']
+    posters = df["poster_path"]
     image_scrape(posters)
--- preprocessing.py
+++ preprocessing.py
@@ -63,6 +63,31 @@ def processing(data):
     display(original, image, 'Original', 'Blured')
     # ---------------------------------
 
+    # Segmentation
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+    # Displaying segmented images
+    display(original, thresh, 'Original', 'Segmented')
+
+    # Further noise removal
+    kernel = np.ones((3, 3), np.uint8)
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
+
+    # sure background area
+    sure_bg = cv2.dilate(opening, kernel, iterations=3)
+
+    # Finding sure foreground area
+    dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
+    ret, sure_fg = cv2.threshold(dist_transform, 0.7 * dist_transform.max(), 255, 0)
+
+    # Finding unknown region
+    sure_fg = np.uint8(sure_fg)
+    unknown = cv2.subtract(sure_bg, sure_fg)
+
+    #Displaying segmented back ground
+    display(original, sure_bg, 'Original', 'Segmented Background')
+
 
 ## body
 data = loadImages(image_path)
