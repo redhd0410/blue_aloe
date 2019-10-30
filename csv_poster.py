291d40e299df943b5eb051393d30261a02431c5a
Jiwon Cha <jc4va@virginia.edu>
2019-10-30 13:55:20 +0900

    None에 에러나는 현상 수정

--- csv_poster.py
+++ csv_poster.py
@@ -9,10 +9,14 @@ def image_scrape(posters_df):
     # 먼저 directory 안에 posters_data라는 폴더를 만들어 주세요! 
     os.chdir(os.getcwd() + "/posters_data")
     for i in range(posters_count):
-        urllib.request.urlretrieve(base_url + posters_df[i][1:], posters_df[i][1:])
+        if (len(posters_df[i]) == 32):
+            urllib.request.urlretrieve(base_url + posters_df[i][1:], posters_df[i][1:])
+        else:
+            continue
 
 
 if __name__ == '__main__':
-    df = pd.read_csv('movies_metadata.csv')
+    df = pd.read_csv('movies_metadata.csv', keep_default_na = False)
     posters = df["poster_path"]
+    posters_count = posters.count()
     image_scrape(posters)
