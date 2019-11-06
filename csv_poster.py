fb6910eab3a781001f48d064d7a2b78e090b399b
jc4va <31323449+jc4va@users.noreply.github.com>
2019-11-06 17:44:14 +0900

    Update csv_poster.py

--- csv_poster.py
+++ csv_poster.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import os
 import urllib
-
+import boto3
+import requests
 
 def image_scrape(posters_df):
     base_url = "http://image.tmdb.org/t/p/w185/"
@@ -9,11 +10,18 @@ def image_scrape(posters_df):
     # 먼저 directory 안에 posters_data라는 폴더를 만들어 주세요! 
     os.chdir(os.getcwd() + "/posters_data")
     for i in range(posters_count):
+        poster_url = base_url + posters_df[i][1:]
+        print("running...." + poster_url)
         if (len(posters_df[i]) == 32):
-            urllib.request.urlretrieve(base_url + posters_df[i][1:], posters_df[i][1:])
+            try:
+                urllib.request.urlretrieve(poster_url, posters_df[i][1:])
+            except requests.exceptions.RequestException as e:
+                continue
+        elif (os.path.isfile(posters_df[i][1:])):
+            print("file is already in the directory.")
+            continue
         else:
             continue
-
 
 if __name__ == '__main__':
     df = pd.read_csv('movies_metadata.csv', keep_default_na = False)
